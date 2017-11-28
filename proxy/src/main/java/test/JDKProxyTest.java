3f820a41b618c8668401dba8bcf3c71b27051402
majiabao <majiabaoIT@163.com>
2017-11-28 23:49:38 +0800

    C

--- proxy/src/main/java/test/JDKProxyTest.java
+++ proxy/src/main/java/test/JDKProxyTest.java
@@ -4,8 +4,6 @@ import bean.HelloWorld;
 import bean.HelloWorldImpl;
 import bean.MyInvocationHandler;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 
@@ -15,17 +13,23 @@ import java.lang.reflect.Proxy;
 public class JDKProxyTest {
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         //这里有两种写法，我们采用略微复杂的一种写法，这样更有助于大家理解。
-        Class<?> proxyClass = Proxy.getProxyClass(JDKProxyTest.class.getClassLoader(), HelloWorld.class);
+        /*Class<?> proxyClass = Proxy.getProxyClass(JDKProxyTest.class.getClassLoader(), HelloWorld.class);
         final Constructor<?> cons = proxyClass.getConstructor(InvocationHandler.class);
         final InvocationHandler ih = new MyInvocationHandler(new HelloWorldImpl());
         HelloWorld helloWorld = (HelloWorld) cons.newInstance(ih);
-        helloWorld.sayHello();
-
+        helloWorld.sayHello();*/
+        //跟踪源码，设置属性值为ture，可以保存代理类字节码
+        System.setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
         //下面是更简单的一种写法，本质上和上面是一样的
         HelloWorld helloWorld2 = (HelloWorld) Proxy.newProxyInstance(
                 JDKProxyTest.class.getClassLoader(),
                 new Class<?>[]{HelloWorld.class},
                 new MyInvocationHandler(new HelloWorldImpl()));
         helloWorld2.sayHello();
+
+       /* HelloWorld helloWorld3 = (HelloWorld) Proxy.newProxyInstance(
+                JDKProxyTest.class.getClassLoader(),
+                new Class<?>[]{HelloWorld.class},
+                new MyInvocationHandler(new HelloWorldImpl()));*/
     }
 }
